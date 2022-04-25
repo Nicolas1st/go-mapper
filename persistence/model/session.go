ae9e0a07538baf511d6d85c4268019dc008ed23b
Nicolas <nicolashereigo@gmail.com>
2022-04-25 20:00:32 +0300

    Added session model

---
+++ persistence/model/session.go
@@ -0,0 +1,22 @@
+package model
+
+import "time"
+
+const ExpiryPeriod time.Duration = 5 * time.Minute
+
+type Session struct {
+	UserID    uint
+	ExpiresAt time.Time
+}
+
+func NewSession(userID uint) *Session {
+	return &Session{
+		UserID: userID,
+		// keep the user logged in for 5 minutes
+		ExpiresAt: time.Now().Add(ExpiryPeriod),
+	}
+}
+
+func (s *Session) IsExpired() bool {
+	return s.ExpiresAt.Before(time.Now())
+}
