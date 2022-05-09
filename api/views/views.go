e6ac27fede848db2ae4bfc0f513c2c2d70dbb2d4
Nicolas <nicolashereigo@gmail.com>
2022-05-09 16:36:26 +0300

    Added the corresponding views

--- api/views/views.go
+++ api/views/views.go
@@ -2,16 +2,16 @@ package views
 
 import (
 	"net/http"
-	"yaroslavl-parkings/web/templates"
+	"yaroslavl-parkings/api/views/templates"
 )
 
 type viewsResource struct {
 	templates *templates.Templates
 }
 
-func newViewsResource(pathToTemplates string) *viewsResource {
+func NewViews(templates *templates.Templates) *viewsResource {
 	return &viewsResource{
-		templates: templates.NewTemplates(pathToTemplates),
+		templates: templates,
 	}
 }
 
@@ -23,6 +23,14 @@ func (viewsResource *viewsResource) SignUpView(w http.ResponseWriter, r *http.Re
 	viewsResource.templates.SignUpPage.Execute(w)
 }
 
-func (viewsResource *viewsResource) makeOrderView(w http.ResponseWriter, r *http.Request) {
+func (viewsResource *viewsResource) MakeOrderView(w http.ResponseWriter, r *http.Request) {
 	viewsResource.templates.MakeOrderPage.Execute(w)
+}
+
+func (viewsResource *viewsResource) AddParkingPlaceView(w http.ResponseWriter, r *http.Request) {
+	viewsResource.templates.AddParkingPage.Execute(w)
+}
+
+func (viewsResource *viewsResource) RemoveParkingPlaceView(w http.ResponseWriter, r *http.Request) {
+	viewsResource.templates.RemoveParkingPage.Execute(w)
 }
