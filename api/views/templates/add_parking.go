68bb4cb4fa92a506ca53a0dcaa7a507c98086486
Nicolas <nicolashereigo@gmail.com>
2022-05-09 16:35:35 +0300

    Added add and remove parkings golang templates

---
+++ api/views/templates/add_parking.go
@@ -0,0 +1,35 @@
+package templates
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+	"path"
+)
+
+type addParkingPage struct {
+	template *template.Template
+}
+
+func NewAddParkingPage(pathToTemplates string) *addParkingPage {
+	templateFileNames := []string{"layout.html", "admin/parkings/add-parking-place.html", "signed-in-navbar.html", "footer.html"}
+	for i, fileName := range templateFileNames {
+		templateFileNames[i] = path.Join(pathToTemplates, fileName)
+	}
+
+	template := template.New("layout")
+
+	template, err := template.ParseFiles(templateFileNames...)
+	if err != nil {
+		fmt.Println(err)
+		panic("Could not build the RemoveParking Page template")
+	}
+
+	return &addParkingPage{
+		template: template,
+	}
+}
+
+func (page *addParkingPage) Execute(w http.ResponseWriter) {
+	page.template.Execute(w, nil)
+}
--- api/views/templates/templates.go
+++ api/views/templates/templates.go
@@ -1,15 +1,19 @@
 package templates
 
 type Templates struct {
-	MakeOrderPage *makeOrderPage
-	SignInPage    *signInPage
-	SignUpPage    *signUpPage
+	MakeOrderPage     *makeOrderPage
+	SignInPage        *signInPage
+	SignUpPage        *signUpPage
+	AddParkingPage    *addParkingPage
+	RemoveParkingPage *removeParkingPage
 }
 
 func NewTemplates(pathToTemplates string) *Templates {
 	return &Templates{
-		MakeOrderPage: NewMakeOrderPage(pathToTemplates),
-		SignInPage:    NewSignInPage(pathToTemplates),
-		SignUpPage:    NewSignUpPage(pathToTemplates),
+		MakeOrderPage:     NewMakeOrderPage(pathToTemplates),
+		SignInPage:        NewSignInPage(pathToTemplates),
+		SignUpPage:        NewSignUpPage(pathToTemplates),
+		AddParkingPage:    NewAddParkingPage(pathToTemplates),
+		RemoveParkingPage: NewRemoveParkingPage(pathToTemplates),
 	}
 }
