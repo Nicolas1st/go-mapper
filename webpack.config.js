616da7e7b6c90c4017f8592aef74a9b7ef56a12a
Nicolas <nicolashereigo@gmail.com>
2022-05-14 18:35:55 +0300

    Updated webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,8 @@
 const path = require('path');
 
-// module.exports = {
-//   entry: './web/js/main.js',
-//   output: {
-//     filename: 'index.js',
-//     path: path.resolve(__dirname, 'web/js'),
-//   },
-//   mode: 'development'
-// };
-
 module.exports = {
   entry: {
-    main: './web/js/main.js',
+    order: './web/js/order.js',
     addParkingPlace: './web/js/add-parking-place.js',
     removeParkingPlace: './web/js/remove-parking-place.js',
   },
