8bf5469808c2f910ed9fcf8241606220eb1a025e
Nicolas <nicolashereigo@gmail.com>
2022-05-18 07:43:32 +0300

    Refactor

--- api/orders/make_order.go
+++ api/orders/make_order.go
@@ -17,6 +17,7 @@ import (
 // creates a payment in the qiwi system,
 // returns the link to the payment page
 func (d *ordersDependencies) makeOrder(w http.ResponseWriter, r *http.Request) {
+	// redirect if not auth
 	if !api.IsAuth(d.sessions, r) {
 		http.Redirect(w, r, api.DefaultEndpoints.OrderPage, http.StatusSeeOther)
 		return
@@ -28,11 +29,28 @@ func (d *ordersDependencies) makeOrder(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	startHour, _ := strconv.Atoi(r.PostFormValue("start-hour"))
-	startMinute, _ := strconv.Atoi(r.PostFormValue("start-minute"))
+	// extract values from the form
+	startHour, err := strconv.Atoi(r.PostFormValue("start-hour"))
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	startMinute, err := strconv.Atoi(r.PostFormValue("start-minute"))
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
 
-	endHour, _ := strconv.Atoi(r.PostFormValue("end-hour"))
-	endMinute, _ := strconv.Atoi(r.PostFormValue("end-minute"))
+	endHour, err := strconv.Atoi(r.PostFormValue("end-hour"))
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	endMinute, err := strconv.Atoi(r.PostFormValue("end-minute"))
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
 
 	// compute for how long the user is going to use the parkingr
 	var duration float64 = (float64(endHour*60+endMinute) - float64(startHour*60+startMinute)) / 60
@@ -61,10 +79,12 @@ func (d *ordersDependencies) makeOrder(w http.ResponseWriter, r *http.Request) {
 	// compute the check
 	sum := int(duration * float64(pricePerHour) * float64(100-discount) / 100)
 
-	// after this time it won't possible to complete the order
+	// get the qiwi payment url
 	timeOut := time.Now().Add(5 * time.Minute)
 	qiwiID := uuid.NewString()
 	qiwiPaymentFormURL, _ := d.paymenter.CreateNewBill(qiwiID, sum, qiwi.RUB, timeOut, "Parking App")
+
+	// record the information about th order
 	orderID, _ := d.orderDB.CreateOrder(*session.User, uint(sum), qiwiPaymentFormURL, qiwiID)
 
 	http.Redirect(w, r, api.DefaultEndpoints.PaymentPage+fmt.Sprintf("?orderID=%v", orderID), http.StatusSeeOther)
