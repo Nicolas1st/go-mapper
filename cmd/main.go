c3c1dbd29b01899b1172e3dba7e391993184cb3d
Nicolas <nicolashereigo@gmail.com>
2022-05-18 13:05:47 +0300

    Updated main.go

--- cmd/main.go
+++ cmd/main.go
@@ -15,29 +15,37 @@ import (
 	"yaroslavl-parkings/api/views"
 	"yaroslavl-parkings/data"
 	"yaroslavl-parkings/data/sessionstorer"
-	"yaroslavl-parkings/pkg/biller"
 	"yaroslavl-parkings/pkg/qiwi"
 )
 
-func main() {
-	// get private key
-	privateKey, exists := os.LookupEnv("qiwiKey")
+func getEnvVar(key string) string {
+	envvar, exists := os.LookupEnv(key)
 	if !exists {
-		panic("must have a qiwi privateKey to launch the application")
+		panic("provide the " + key + " env var")
 	}
 
+	return envvar
+}
+
+func main() {
+	// read in the env vars
+	APP_QIWIKEY := getEnvVar("APP_QIWIKEY")
+	APP_DATABASE_HOST := getEnvVar("PP_DATABASE_HOST")
+	APP_DATABASE_PORT := getEnvVar("PP_DATABASE_PORT")
+	APP_DATABASE_PASSWORD := getEnvVar("PP_DATABASE_PASSWORD")
+	APP_DATABASE_NAME := getEnvVar("PP_DATABASE_NAME")
+	APP_DATABASE_USER := getEnvVar("PP_DATABASE_USER")
+
 	// init qiwi
-	paymenter := qiwi.NewPaymenter(privateKey)
+	paymenter := qiwi.NewPaymenter(APP_QIWIKEY)
 
 	// init database
-	dsn := "host=localhost user=postgres password=password dbname=parkings port=5432 sslmode=disable"
+	dsn := fmt.Sprintf("host=%v user=%v password=%v dbname=%v port=%v sslmode=disable", APP_DATABASE_HOST, APP_DATABASE_USER, APP_DATABASE_PASSWORD, APP_DATABASE_NAME, APP_DATABASE_PORT)
 	db := data.NewDatabase(dsn)
 	db.InitTables()
 
-	// create adming user if not already created
+	// set up initial app state if not already set up
 	db.User.SetUpAdminAccount("admin", "admin@admin.admin", "admin", 25)
-
-	// set up tarrifs if not set up already
 	db.Rate.SetUpAgeCategoryBasedHourlyRates(20, 15)
 	db.Rate.SetUpDiscount(0, 20)
 
@@ -48,13 +56,8 @@ func main() {
 	files := http.FileServer(http.Dir("./web"))
 	http.Handle("/static/", http.StripPrefix("/static/", files))
 
-	// create views
-	views := views.NewViews("api/views/html", sessionStorage, db.Rate, db.Order, paymenter)
-
-	// init authentication functions
-	auth := auth.NewAuthHandlers(sessionStorage, db.User)
-
-	// set up routes for views
+	// set up views
+	views := views.NewViews("./api/views/html", sessionStorage, db.Rate, db.Order, paymenter)
 	http.HandleFunc(api.DefaultEndpoints.Root, views.SignIn)
 	http.HandleFunc(api.DefaultEndpoints.LoginPage, views.SignIn)
 	http.HandleFunc(api.DefaultEndpoints.SignUpPage, views.SignUp)
@@ -71,35 +74,42 @@ func main() {
 	http.HandleFunc(api.DefaultEndpoints.PaymentPage, views.PaymentPage)
 	http.HandleFunc(api.DefaultEndpoints.OrdersPage, views.OrdersPage)
 
-	// account handlers
+	// set up account handlers
 	accounts := personal.NewPersonalDataApi(db.User, sessionStorage)
 	signupResultRedirector := middlewares.BuildRedirectOnApiCallResult(api.DefaultEndpoints.LoginPage, api.DefaultEndpoints.SignUpPage)
 	http.HandleFunc(api.DefaultEndpoints.CreateAccountHandler, signupResultRedirector(accounts.CreateAccount))
 
-	// setup authentication
-	lougoutResultRedirector := middlewares.BuildRedirectOnApiCallResult(api.DefaultEndpoints.LoginPage, api.DefaultEndpoints.OrderPage)
+	// set up auth functions
+	auth := auth.NewAuthHandlers(sessionStorage, db.User)
+
+	lougoutResultRedirector := middlewares.BuildRedirectOnApiCallResult(
+		api.DefaultEndpoints.LoginPage,
+		api.DefaultEndpoints.OrderPage,
+	)
 	http.HandleFunc(api.DefaultEndpoints.LogoutHandler, lougoutResultRedirector(auth.LogoutHandler))
 
-	authenticationResultRedirector := middlewares.BuildRedirectOnApiCallResult(api.DefaultEndpoints.OrderPage, api.DefaultEndpoints.LoginPage)
+	authenticationResultRedirector := middlewares.BuildRedirectOnApiCallResult(
+		api.DefaultEndpoints.OrderPage,
+		api.DefaultEndpoints.LoginPage,
+	)
 	http.HandleFunc(api.DefaultEndpoints.AuthenticateHandler, authenticationResultRedirector(auth.LoginHandler))
 
-	// add parkings api
+	// set up parkings api
 	parkingsApi := parkings.NewParkingsApi(db.Parking)
 	http.Handle("/parkings/", http.StripPrefix("/parkings", parkingsApi))
 
-	// add rates api
-	ratesApiRedirector := middlewares.BuildRedirectOnApiCallResult(api.DefaultEndpoints.SeePricings, api.DefaultEndpoints.SeePricings)
+	// set up rates api
 	ratesApi := rates.NewRatesHandlers(db.Rate, sessionStorage)
+	ratesApiRedirector := middlewares.BuildRedirectOnApiCallResult(api.DefaultEndpoints.SeePricings, api.DefaultEndpoints.SeePricings)
 	http.HandleFunc(api.DefaultEndpoints.ChangeActiveHoursDiscountHandler, ratesApiRedirector(ratesApi.UpdateActiveHoursDiscount))
 	http.HandleFunc(api.DefaultEndpoints.ChangeSluggishHoursDiscountHandler, ratesApiRedirector(ratesApi.UpdateSluggishHoursDiscount))
 	http.HandleFunc(api.DefaultEndpoints.ChangeAdultBaseRateHandler, ratesApiRedirector(ratesApi.UpdateAdultHourlyRate))
 	http.HandleFunc(api.DefaultEndpoints.ChangeRetireeBaseRateHandler, ratesApiRedirector(ratesApi.UpdateRetireeHourlyRate))
 
-	// add orders api
-	biller := biller.NewBiller()
-	orders := orders.NewOrdersApi(db.Order, db.Rate, sessionStorage, paymenter, biller, db.Parking)
+	// set up orders api
+	orders := orders.NewOrdersApi(db.Order, db.Rate, sessionStorage, paymenter, db.Parking)
 	http.Handle(api.DefaultEndpoints.CreateOrderHandler, orders.MakeOrder)
 
 	fmt.Println("Server started on http://localhost:8880")
-	http.ListenAndServe(":8880", nil)
+	fmt.Println(http.ListenAndServe(":8880", nil))
 }
